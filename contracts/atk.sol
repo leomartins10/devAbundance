a3833ee33cdf411cb015351298d55f2a38331be4
moysesanijar <moyses.anijar@sou.inteli.edu.br>
2025-02-20 17:56:52 -0300

    commit MA

--- contracts/atk.sol
+++ contracts/atk.sol
@@ -5,6 +5,28 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract ATK is ERC20, Ownable(msg.sender) {
-    constructor() ERC20("Abundance Token", "ATK") { 
+    uint256 public totalEmitido;
+
+    constructor() ERC20("Abundance Token", "ATK") {
+    }
+
+    function emitirTokens(uint256 quantidade) external onlyOwner {
+        _mint(owner(), quantidade);
+        totalEmitido += quantidade;
+    }
+
+    function _beforeTokenTransfer(address de, address para, uint256 quantidade) internal  {
+        super._beforeTokenTransfer(de, para, quantidade);
+        if (de == address(0) || para == address(0)) {
+            return;
+        }
+        if (de == owner()) {
+            uint256 saldoDepois = balanceOf(owner()) - quantidade;
+            uint256 minimoNecessario = (totalEmitido * 30) / 100;
+            require(
+                saldoDepois >= minimoNecessario,
+                "ATK: Proprietario deve manter no minimo 30% dos tokens emitidos."
+            );
+        }
     }
 }
